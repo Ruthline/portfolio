931a1a734920de96d7de99915c86f165b8bd3a44
Ruthline <rutheveline@hotmail.es>
2022-08-31 10:22:04 -0500

    Contain about me alredy responsive, waiting settings...

---
+++ src/components/mainProfile/MainProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../mainProfile/MainProfile.css'
+
+function MainProfile() {
+  return (
+    <main>
+        <div className='container-main'>
+            <h2>About me</h2>
+            <div className='paragraph'>
+                <p>I am an empirical programmer, throughout my career in Frontend programming my approach has been to replicate designs and allow the user to intuit the usability of the site.</p>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default MainProfile;
--- src/pages/aboutMe/Home.jsx
+++ src/pages/aboutMe/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
+import MainProfile from '../../components/mainProfile/MainProfile';
 import Menu from '../../components/menu/Menu';
 
 function Home() {
   return (
-    <div>
+    <div className='Home'>
         <Menu/>
         <Header/>
+        <MainProfile/>
         <Footer/>
     </div>
   )
